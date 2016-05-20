08bb6288c0c3a4a9401b128aae87410631b3fb26
Charles Skariah <charles2101@gmail.com>
2016-05-20 19:27:53 +0000

    required_config_and_gems_added

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -4,4 +4,18 @@ class EmployersController < ApplicationController
     def index
     end
     
+    def edit
+      @employer = Employer.find(params[:id])
+    end
+    
+    def update 
+      @employer = Employer.find(params[:id]) 
+      if @employer.update_attributes(params.require(:employer).permit(:email)) 
+        redirect_to edit_employer_path, :flash => { :success => "success" }
+      else
+        flash[:error] = @employer.errors.full_messages[0]
+        redirect_to edit_employer_path
+      end
+   end
+    
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,21 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   
   #devise-mailer
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 8080 }
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "employercode.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none',
+    user_name: "dragthemm@gmail.com",
+    password: "dragthem2101"
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
